da8b4f3de03bc62c870595b881ab5af7b8142b86
AaronMax1 <aaron1234156@outlook.com>
2024-07-01 23:07:51 +0800

    init

---
+++ src/app/api/actions/donate/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "Cuigpd5P2LWTw1KDLWRuyCnA5xFuwUPEZhhZzr54ZL33" // donate wallet
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 0.01;
